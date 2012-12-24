971d8940461b669c906f9797d7e370d6edc2052b
John Griffiths <john@johnantoni.com>
2012-12-24 18:17:53 -0500

    fin

--- lib/glowing-archer.rb
+++ lib/glowing-archer.rb
@@ -1,7 +1,26 @@
 module Glowing
   module Archer
-    # code goes here
+
+    def bootstrap_badges(badges, style = "")
+      style = (" " + style) unless style.nil_or_empty?
+      html = ""
+      badges.split(",").each do |badge|
+        badge = badge.strip
+        html << "<span class=\"label#{style}\">#{badge.html_safe!}</span> " unless badge.nil_or_empty?
+      end
+      return html.html_safe
+    end
+
   end
 end
 
-require 'rails/array'
+require 'find'
+
+# load all extensions
+extensions_directory = File.expand_path("glowing-archer/rails", File.dirname(__FILE__))
+
+Find.find(extensions_directory) do |path|
+   next if File.extname(path) != ".rb"
+   #puts "loading core extension: #{path}"
+   require path
+end
