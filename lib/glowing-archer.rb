e9053cd0b6f0455b1bb4a08aacf433b4c0b2417b
John Antoni Griffiths <john@uxgent.co>
2013-02-03 20:58:56 -0500

    Update lib/glowing-archer.rb

--- lib/glowing-archer.rb
+++ lib/glowing-archer.rb
@@ -1,7 +1,7 @@
 module Glowing
   module Archer
 
-    # generate invite code http://railscasts.com/episodes/124-beta-invitations
+    # generate invite code #1 http://railscasts.com/episodes/124-beta-invitations
     # => "bf35184b0e7ebb5baad116bfe4a8dbdfb58277d2"
     def self.build_invite
       Digest::SHA1.hexdigest([Time.now, rand].join)
@@ -13,7 +13,10 @@ module Glowing
       SecureRandom.base64(length).tr('+/=', '0aZ').strip.delete("\n")
     end
     
+    # generate secure random guid #3 http://rubydoc.info/stdlib/securerandom/1.9.2/SecureRandom#uuid-class_method
+    # => "ee705896-1d41-415c-a152-55687d0bc8c9"
     def self.guid
+      SecureRandom.uuid
     end
     
   end
